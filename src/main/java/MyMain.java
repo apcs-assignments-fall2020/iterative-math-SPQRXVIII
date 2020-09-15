58211244095094370650f4a2ffa27722283b0b71
SPQRXVIII <Aftersilver001@gmail.com>
2020-09-15 04:02:23 +0000

    finished

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,33 +4,44 @@ public class MyMain {
     
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double n = Math.random();
+        while(Math.abs(n - (n + x/n) / 2 )> 0.00001){
+            n = (n + x/n) / 2;
+        }
+        
+        return n;
     }
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int n = 1;
+        while(x>0){
+            n = x*n;
+            x -= 1;
+        }
+        return n;
     }
 
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double E_0 = 1;
+        int n = 1;
+        while(Math.E - E_0 > 0.00000000001){
+            E_0 += 1/factorial(n);
+            n += 1;
+        }
+        return E_0;
     }
     
     
     public static void main(String[] args) {
+        System.out.println("Enter a positive number!");
         Scanner scan = new Scanner(System.in);
-
-        // YOUR CODE HERE
-
-
-        
-
+        double x = scan.nextDouble();
+        System.out.println("The squareroot of "+ x +" is "+babylonian(x));
+        System.out.println("The value of e is roughly : "+calculateE());
         scan.close();
     }
 }
